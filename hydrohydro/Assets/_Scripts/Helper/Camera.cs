d69d6b8ca685cd2faf9d24dae77a4b6f36d370dd
Fred Newton <ghostlopment@gmail.com>
2020-08-03 21:14:43 +0200

    camera follow

---
+++ hydrohydro/Assets/_Scripts/Helper/Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [HideInInspector] public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void LateUpdate()
+    {
+        if (target != null)
+        {
+            Vector3 deiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, deiredPosition, smoothSpeed);
+            transform.position = smoothedPosition;
+        }
+    }
+}
